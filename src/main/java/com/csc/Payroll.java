42a1cb50a9b55bd8df5d47a1a338b4cc3c470946
jgraessle <j_graessle@mail.harpercollege.edu>
2024-09-22 18:34:41 -0700

    New Payed Fees Checker and Input for Hourly Pay
    The Payed Fees Checker addition to this program ensures that the net pay displayed to the user is always positive but if the fees weren't fully paid, they will be listed as still being owed. The new hourly rate input replaced the constant $16.78 by swapping the constant HOURRATE with a variable inputRate that is prompted for from the user. The inputRate is also ensured to always be positive via a while loop addition that checks it is $0.00 or greater and prompts the user for a new hourly rate if that isn't the case.

--- src/main/java/com/csc/Payroll.java
+++ src/main/java/com/csc/Payroll.java
@@ -7,17 +7,21 @@ public class Payroll {
   // Variables and Constants used across Payroll Functions
   double hours = 0;
   int dependents = 0;
-  final double HOURRATE = 16.78;
+  double inputRate = 16.78;
 
-  // Constants for Taxes Function
+  // Constants and Variables for Taxes Function
   final double SOCSECTAX = 0.06;
   final double FEDTAX = 0.14;
   final double STATAX = 0.05;
+  double socsecTax;
+  double fedTax;
+  double staTax;
 
-  // Constants for Fees Function
+  // Constants and Variables for Fees Function
   final double UNIONDUES = 10.00;
   final int INSURANCE3FEE = 35;
   final int INSURANCEFEE = 15;
+  double insuranceFee;
 
   public Payroll(double hours, int dependents) {
     this.hours = hours;
@@ -29,10 +33,10 @@ public class Payroll {
     double hourRate;
     if (hours > 40) {
       double overtime = hours - 40;
-      hourRate = ((HOURRATE * hours) + (HOURRATE * overtime * 0.5)) / (hours);
+      hourRate = ((inputRate * hours) + (inputRate * overtime * 0.5)) / (hours);
     }
     else {
-      hourRate = HOURRATE;
+      hourRate = inputRate;
     }
     return hourRate;
   }
@@ -42,10 +46,10 @@ public class Payroll {
     double grossEarnings;
     if (hours > 40) {
       double overtime = hours - 40;
-      grossEarnings = HOURRATE * (overtime * 0.5 + hours);
+      grossEarnings = inputRate * (overtime * 0.5 + hours);
     }
     else {
-      grossEarnings = (HOURRATE * hours);
+      grossEarnings = (inputRate * hours);
     }
       return grossEarnings;
   } 
@@ -53,15 +57,15 @@ public class Payroll {
   //Calculation Function for Taxes
   public double Taxes() {
     double grossEarnings = GrossEarnings();
-    double socsecTax = grossEarnings * SOCSECTAX;
-    double fedTax = grossEarnings * FEDTAX;
-    double staTax = grossEarnings * STATAX;
+    socsecTax = grossEarnings * SOCSECTAX;
+    fedTax = grossEarnings * FEDTAX;
+    staTax = grossEarnings * STATAX;
     return socsecTax + fedTax + staTax;
   }
 
   //Calculation Function for Fees
   public double Fees() {
-    double insuranceFee = INSURANCEFEE;
+    insuranceFee = INSURANCEFEE;
     if (dependents >= 3) {
       insuranceFee = INSURANCE3FEE;
     }
@@ -86,19 +90,47 @@ public static void main(String[] args) {
 
   //Initial Prompting the User
   Scanner scanner = new Scanner(System.in);
+
   System.out.print("Enter the amount of hours worked this week: ");
   double hours = scanner.nextDouble();
   System.out.print("Enter the amount of dependents for insurance purposes: ");
   int dependents = scanner.nextInt();
-  scanner.close();
 
+  // New input for the User's hourly rate
   Payroll payroll = new Payroll(hours, dependents);
+  System.out.print("Enter the hourly rate you earn: $");
+  payroll.inputRate = scanner.nextDouble();
+  
+  //New Checker that the User's hourly rate is positive
+  while (payroll.inputRate < 0) {
+    System.out.print("Please input an hourly rate you earn that is $0.00 or greater: $");
+    payroll.inputRate = scanner.nextDouble();
+  }
+  scanner.close();
+  
 
   //Printing the results out
-  System.out.println("Hours worked:   " + String.format("%5.2f", payroll.hours));
-  System.out.println("Hourly rate:    " + String.format("$%5.2f", payroll.HourRate()));
-  System.out.println("Gross earnings: " + String.format("$%5.2f", payroll.GrossEarnings()));
-  System.out.println("Total expenses: " + String.format("$%5.2f", payroll.Expenses()));
-  System.out.println("Net pay:        " + String.format("$%5.2f", payroll.NetPay()));
+  System.out.println("Hours worked:        " + String.format("%4.2f", payroll.hours));
+  System.out.println("Hourly rate:         " + String.format("$%5.2f", payroll.HourRate()));
+  System.out.println("Gross earnings:      " + String.format("$%5.2f", payroll.GrossEarnings()));
+  payroll.Taxes();
+  payroll.Fees();
+  System.out.println("Social Security Tax: " + String.format("$%5.2f", payroll.socsecTax));
+  System.out.println("State Tax:           " + String.format("$%5.2f", payroll.staTax));
+  System.out.println("Federal Tax:         " + String.format("$%5.2f", payroll.fedTax));
+
+  // New check to ensure Fees are payed and User doesn't have a net negative
+  if (payroll.NetPay() < 0) {
+    double newNet = payroll.NetPay() + payroll.insuranceFee + payroll.UNIONDUES;
+    System.out.println("Net pay:             " + String.format("$%5.2f", newNet));
+    System.out.println("You still owe:");
+    System.out.println("Insurance Fee        " + String.format("$%5.2f", payroll.insuranceFee));
+    System.out.println("Union Dues:          " + String.format("$%5.2f", payroll.UNIONDUES));
+  }
+  else {
+  System.out.println("Insurance Fee        " + String.format("$%5.2f", payroll.insuranceFee));
+  System.out.println("Union Dues:          " + String.format("$%5.2f", payroll.UNIONDUES));
+  System.out.println("Net pay:             " + String.format("$%5.2f", payroll.NetPay()));
+  }
 }
 }
